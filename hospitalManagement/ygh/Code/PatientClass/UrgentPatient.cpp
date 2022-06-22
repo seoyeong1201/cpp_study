48189d4dbb3b75f7dc24ff018ade44baf56bf504
redcourage <ongddrcg@gmail.com>
2022-06-22 21:53:27 +0900

    remove ygh directory

--- hospitalManagement/ygh/Code/PatientClass/UrgentPatient.cpp
+++
@@ -1,8 +0,0 @@
-#include "UrgentPatient.h"
-
-void UrgentPatient::showPatientInfo() const {
-    Patient::showPatientInfo();
-    cout << "Patient state : " << getState() << endl;
-}
-
-string UrgentPatient::getState() const { return patientState; }
